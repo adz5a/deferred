8d942f907e75ce6ea6aa9f5741bc5da8cbbafcdf
adz5a <adz5a@gmail.com>
2016-03-05 10:45:54 +0100

    remove unneeded methods

--- deferred.js
+++ deferred.js
@@ -9,24 +9,6 @@ var deferred = module.exports = exports = function () {
     return new core.Deferred();
 };
 
-/**
- *
- * @param promise {Deferred}
- * @param reason {*}
- */
-deferred.reject  = function ( promise, reason ) {
-    return core.Deferred.prototype.reject.call( promise, reason );
-};
-
-/**
- *
- * @param promise {Deferred}
- * @param value {*}
- */
-deferred.resolve = function ( promise, value ) {
-    return core.Deferred.prototype.resolve.call( promise, value );
-};
-
 
 deferred.getConstructor = require( "./lib/exportConstructor.js" );
 deferred.all            = require( "./lib/all.js" );
