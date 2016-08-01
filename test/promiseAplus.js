cbfcd3d6a9902c42d65c721708329b5c4ca209d8
antoine.zimmermann@storetail.com <antoine.zimmermann@storetail.com>
2016-08-01 12:40:32 +0200

    refactor the tests to add the constructor

--- test/promiseAplus.js
+++ test/promiseAplus.js
@@ -1,7 +1,12 @@
 "use strict";
 var promisesAplusTests = require( "promises-aplus-tests" );
 var adapter = require( "./adapter" );
+var constructorAdapter = require( "./promise-constructor-adapter.js" );
 
 promisesAplusTests( adapter, function ( err ) {
+    // All done; output is in the console. Or check `err` for number of failures.
+} );
+
+promisesAplusTests( constructorAdapter, function ( err ) {
     // All done; output is in the console. Or check `err` for number of failures.
 } );
